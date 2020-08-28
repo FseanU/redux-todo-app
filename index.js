320bac4633c19a2c47407297d2d775d73eca3dc1
Sean Fu <sean14xx@gmail.com>
2020-08-28 14:58:28 +0200

    refactor

--- index.js
+++ index.js
@@ -32,15 +32,30 @@ function createStore (reducer) {
 
 // App Code
 function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state;
   }
-
-  return state;
 }
 
 const store = createStore(todos);
 
 store.subscribe(() => {
   console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
 })

a4611e4a90a60affdecb69a95a44395fe40c5fdf
Sean Fu <sean14xx@gmail.com>
2020-08-28 15:05:25 +0200

    Add goals reducer

--- index.js
+++ index.js
@@ -45,6 +45,15 @@ function todos (state = [], action) {
   }
 }
 
+function goals(state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+  }
+}
+
 const store = createStore(todos);
 
 store.subscribe(() => {

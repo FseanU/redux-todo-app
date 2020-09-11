86babe3ef105b8ea193cc9e380052473aa09a1fa
Sean Fu <sean14xx@gmail.com>
2020-09-11 10:53:46 +0200

    refactor: Move components to component folder

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+  handleInitialData
+} from '../actions/shared'
 
-function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    dispatch(handleInitialData());
+  }
+
+  render() {
+    if (this.props.loading) {
+      return <h3>Loading</h3>;
+    }
+
+    return (
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading,
+}))(App);
+
